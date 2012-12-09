550103c36bfa56ab93638fe5c1fc4d2cf9250149
Leigh MacDonald <leigh@cudd.li>
2012-12-09 02:28:17 -0700

    Added base db structure

---
+++ tranny/db/pickle.py
@@ -0,0 +1,20 @@
+__author__ = 'leigh'
+
+class PickleDB(object):
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def exists(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def size(self):
+        pass
+
+    def fetch(self):
+        pass
