d0dab4488bef1497e2d99f3a5471892aac3fb1ec
Leigh MacDonald <leigh@cudd.li>
2013-08-25 16:10:08 -0600

    Added manual cli upload script

---
+++ scripts/tranny-add.py
@@ -0,0 +1,39 @@
+#!/bin/env python
+"""
+Basic CLI script to manually upload a torrent to the configured backend client
+
+./tranny-add torrent1.torrent torrent2.torrent..
+
+"""
+from os.path import dirname
+from sys import argv, path
+
+path.append(dirname(dirname(__file__)))
+
+from tranny.exceptions import ConfigError
+from tranny.client import init_client
+from tranny.torrent import Torrent
+from tranny.util import file_size
+
+client = init_client()
+
+print("> Connected to {}".format(client))
+try:
+    if len(argv) <= 1:
+        raise ConfigError("! Not enough arguments")
+    torrent_data = []
+    for torrent in argv[1:]:
+        try:
+            torrent_data.append(open(torrent, 'r').read())
+        except IOError as err:
+            print(err)
+    if len(torrent_data) != len(argv[1:]):
+        raise ConfigError("! Failed to locate all files, bailing")
+except ConfigError as err:
+    print(err)
+else:
+    for raw_torrent in torrent_data:
+        torrent_struct = Torrent.from_str(raw_torrent)
+        print("-> {} @ {}".format(torrent_struct['info']['name'], file_size(torrent_struct.size())))
+        client.add(raw_torrent)
+
