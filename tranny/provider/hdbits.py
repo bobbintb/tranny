ae8711856635666d5b1bf3cc81f0ba1cb6817fa6
Leigh MacDonald <leigh@cudd.li>
2014-09-02 15:48:43 -0600

    Added HDB provider skeleton, null sucks.

---
+++ tranny/provider/hdbits.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import unicode_literals, absolute_import
+import requests
+from tranny import provider, app
+from tranny.exceptions import AuthenticationError, ApiError
+
+
+class HDBits(provider.TorrentProvider):
+    """
+    .. _HDB API Docs: https://hdbits.org/wiki/index.php?title=API
+    """
+    def __init__(self, config_section):
+        super(HDBits, self).__init__(config_section)
+        self.endpoint = app.config.get_default(self._config_section, 'endpoint', 'https://hdbits.org/api')
+        self.enabled = app.config.getboolean(self._config_section, 'enabled')
+        self.interval = app.config.get_default(self._config_section, 'interval', self.interval, int)
+
+    def _request(self, method):
+        pass
+
+    def find_matches(self):
+        return []
