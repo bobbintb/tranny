a4c7965e3ba563cfdf3ce79de717593df2d1390b
Leigh MacDonald <leigh@cudd.li>
2014-09-07 03:22:46 -0600

    Started notification skeletons

---
+++ tranny/notification/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+
+def send_notifications(torrent):
+    """ Send notifications over all of the enabled notification systems
+
+    :param torrent:
+    :type torrent:
+    :return: Send status
+    :rtype: bool
+    """
+    pass
