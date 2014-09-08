c39fb9dd4aa16ece6862fc5b8eea6f48cf0678d3
Leigh MacDonald <leigh@cudd.li>
2014-09-07 20:38:39 -0600

    Added gevent task queue for long running background tasks.

---
+++ tranny/tasks.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+import gevent
+from gevent import queue
+
+
+class Task(object):
+    def __init__(self, func, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.func = func
+
+    def __call__(self):
+        self.func(*self.args, **self.kwargs)
+
+
+def consumer():
+    while True:
+        try:
+            task = _task_queue.get()
+        except queue.Empty:
+            pass
+        else:
+            try:
+                task()
+            finally:
+                _task_queue.task_done()
+
+
+def add_task(task):
+    _task_queue.put_nowait(task)
+
+
+_running_tasks = {}
+_task_queue = queue.JoinableQueue()
+_consumer = gevent.spawn(consumer)
