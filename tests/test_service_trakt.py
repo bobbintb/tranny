216aacbb25d4ebc87f8f7cf4117840845b1ab76c
Leigh MacDonald <leigh@cudd.li>
2014-09-08 00:39:41 -0600

    Full test coverage for trakt added. Allow fuzzy matching of show/movie titles against trakt. Cache slug lookups.

---
+++ tests/test_service_trakt.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from testcase import TrannyTestCase
+from tranny.service import trakt
+
+
+class TraktServiceTest(TrannyTestCase):
+    show = 'seinfeld'
+    season = 3
+    episode = 1
+    tvrage_id = 5145
+    movie = 'Reservoir Dogs'
+    imdb_id = 'tt0105236'
+
+    def test_show_summary(self):
+        # Uses title with space to test slug detection
+        self.assertEqual(trakt.show_summary("The Simpsons").get('tvrage_id', 0), 6190)
+
+    def test_show_related(self):
+        self.assertTrue(len(trakt.show_related(self.show)) >= 10)
+
+    def test_show_season(self):
+        season = trakt.show_season(self.show, self.season)
+        self.assertTrue(len(season) == 23)
+
+    def test_show_seasons(self):
+        seasons = trakt.show_seasons(self.show)
+        self.assertEqual(len(seasons), 10)
+
+    def test_show_episode_summary(self):
+        summary = trakt.show_episode_summary(self.show, self.season, self.episode)
+        self.assertEqual(summary.get('episode', {}).get('season', 0), self.season)
+        self.assertEqual(summary.get('episode', {}).get('number', 0), self.episode)
+
+    def test_calendar_shows(self):
+        shows = trakt.calendar_shows()
+        self.assertEqual(len(shows), 7)
+
+    def test_calendar_premiers(self):
+        premiers = trakt.calendar_premiers()
+        self.assertTrue(len(premiers) > 0)
+
+    def test_search_tv(self):
+        result = trakt.search_tv(self.show)
+        self.assertTrue(result)
+        self.assertEqual(result['tvrage_id'], self.tvrage_id)
+
+    def test_search_movie(self):
+        result = trakt.search_movie(self.movie)
+        self.assertTrue(result)
+        self.assertEqual(result['imdb_id'], self.imdb_id)
+
+    def test_movie_summary(self):
+        movie = trakt.movie_summary(self.movie)
+        self.assertEqual(movie.get('imdb_id', ''), self.imdb_id)
+
+    # These require a history on trakt, skip for now.
+    # def test_recommend_movies(self):
+    #     recommendations = trakt.recommend_movies()
+    #    self.assertTrue(recommendations)
+
+    # def test_recommend_shows(self):
+    #    recommendations = trakt.recommend_shows()
+    #    self.assertTrue(recommendations)
--- tranny/service/trakt.py
+++ tranny/service/trakt.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
-from tranny import net
+from functools import partial
+from tranny import net, constants, util
 from tranny.app import config
+
+_slug_cache = {
+    constants.MEDIA_MOVIE: {},
+    constants.MEDIA_TV: {}
+}
+
+
+def _find_slug(title, media_type):
+    slug = _slug_cache[media_type].get(title, None)
+    if slug:
+        return slug
+    else:
+        closest = search(title, media_type)
+        if media_type == constants.MEDIA_MOVIE:
+            slug = closest['imdb_id']
+        elif media_type == constants.MEDIA_TV:
+            slug = closest['tvdb_id']
+        else:
+            return title
+        if slug:
+            _slug_cache[media_type][title] = slug
+        return slug
+
+
+def _make_slug(title):
+    return "-".join(title.lower().split(" "))
 
 
 def _make_url(method, api_key):
@@ -9,10 +36,11 @@ def _make_url(method, api_key):
 
 
 def _make_request(method, *args):
-    key = config.get_default('trakt', 'apikey', None)
+    key = config.get_default('trakt', 'api_key', None)
     if not key:
         return {}
-    return net.fetch_url(_make_url(method, key))
+    full_url = "".join([_make_url(method, key), '/' if args else "", '/'.join(map(unicode, args))])
+    return net.fetch_url(full_url)
 
 
 def calendar_shows():
@@ -23,36 +51,46 @@ def calendar_premiers():
     return _make_request('calendar/premieres')
 
 
-def search(search_query):
-    return _make_request('search/shows', search_query)
+def search(search_query, media_type):
+    if media_type == constants.MEDIA_TV:
+        method = 'search/shows'
+    else:
+        method = 'search/movies'
+    results = _make_request(method, search_query)
+    return util.find_closest_match(search_query, results, 'title')
+
+
+search_tv = partial(search, media_type=constants.MEDIA_TV)
+
+search_movie = partial(search, media_type=constants.MEDIA_MOVIE)
 
 
 def show_episode_summary(show, season, episode):
-    return _make_request('show/episode/summary', show, season, episode)
+    return _make_request('show/episode/summary', _find_slug(show, constants.MEDIA_TV), season, episode)
 
 
 def show_related(show):
-    return _make_request('show/related', show)
+    return _make_request('show/related', _find_slug(show, constants.MEDIA_TV))
 
 
 def show_season(show, season):
-    return _make_request('show/season', show, season)
+    return _make_request('show/season', _find_slug(show, constants.MEDIA_TV), season)
 
 
 def show_seasons(show):
-    return _make_request('show/seasons', show)
+    return _make_request('show/seasons', _find_slug(show, constants.MEDIA_TV))
 
 
 def show_summary(show):
-    return _make_request('show/summary', show)
+    return _make_request('show/summary', _find_slug(show, constants.MEDIA_TV))
 
 
 def movie_summary(movie):
-    return _make_request('movie/summary', movie)
+    return _make_request('movie/summary', _find_slug(movie, constants.MEDIA_MOVIE))
 
 
 def movie_related(movie):
-    return _make_request('movie/related', movie)
+    return _make_request('movie/related', _find_slug(movie, constants.MEDIA_MOVIE))
 
 
 def recommend_movies():
