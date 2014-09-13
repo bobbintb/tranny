c88d83e42767abf29c2d35ae6526133ac5653965
Kannibal Ox <kannibalox@gmail.com>
2014-09-12 20:19:30 -0400

    Fix user query on login check

--- tranny/handlers/user.py
+++ tranny/handlers/user.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from flask import Blueprint, redirect, url_for, request, flash
 from flask.ext.login import logout_user, login_user
 from tranny import models, ui
+from tranny.app import Session
 import hashlib
 
 usr = Blueprint("user", __name__, url_prefix="/user")
@@ -21,7 +22,8 @@ def login_perform():
     except KeyError:
         pass
     else:
-        user = models.User.query.filter_by(user_name=user_name).first()
+        session = Session()
+        user = session.query(models.User).filter_by(user_name=user_name).first()
         if not user or not user.password == hashlib.sha1(user_password).hexdigest():
             flash("Invalid credentials", "alert")
             return redirect(url_for(".login"))
